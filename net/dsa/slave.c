06854291d44acc2a93e2b84fb06ad649eacb1192
Brandon Streiff <brandon.streiff@ni.com>
2017-09-28 10:25:37 -0500

    net: dsa: forward hardware timestamping ioctls to switch driver
    This patch adds support to the dsa slave network device so that
    switch drivers can implement the SIOC[GS]HWTSTAMP ioctls and the
    ethtool timestamp-info interface.
    
    Signed-off-by: Brandon Streiff <brandon.streiff@ni.com>

--- net/dsa/slave.c
+++ net/dsa/slave.c
@@ -279,9 +279,26 @@ dsa_slave_fdb_dump(struct sk_buff *skb, struct netlink_callback *cb,
 static int dsa_slave_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 {
 	struct dsa_slave_priv *p = netdev_priv(dev);
+	struct dsa_switch *ds = p->dp->ds;
+	int port = p->dp->index;
 
-	if (p->phy != NULL)
+	if (!p->phy)
+		return -ENODEV;
+
+	switch (cmd) {
+	case SIOCGMIIPHY:
+	case SIOCGMIIREG:
+	case SIOCSMIIREG:
 		return phy_mii_ioctl(p->phy, ifr, cmd);
+	case SIOCGHWTSTAMP:
+		if (!ds->ops->port_hwtstamp_get)
+			break;
+		return ds->ops->port_hwtstamp_get(ds, port, ifr);
+	case SIOCSHWTSTAMP:
+		if (!ds->ops->port_hwtstamp_set)
+			break;
+		return ds->ops->port_hwtstamp_set(ds, port, ifr);
+	}
 
 	return -EOPNOTSUPP;
 }
@@ -1007,6 +1024,18 @@ static int dsa_slave_set_rxnfc(struct net_device *dev,
 	return ds->ops->set_rxnfc(ds, p->dp->index, nfc);
 }
 
+static int dsa_slave_get_ts_info(struct net_device *dev,
+				 struct ethtool_ts_info *ts)
+{
+	struct dsa_slave_priv *p = netdev_priv(dev);
+	struct dsa_switch *ds = p->dp->ds;
+
+	if (!ds->ops->get_ts_info)
+		return -EOPNOTSUPP;
+
+	return ds->ops->get_ts_info(ds, p->dp->index, ts);
+}
+
 static const struct ethtool_ops dsa_slave_ethtool_ops = {
 	.get_drvinfo		= dsa_slave_get_drvinfo,
 	.get_regs_len		= dsa_slave_get_regs_len,
@@ -1027,6 +1056,7 @@ static const struct ethtool_ops dsa_slave_ethtool_ops = {
 	.set_link_ksettings	= dsa_slave_set_link_ksettings,
 	.get_rxnfc		= dsa_slave_get_rxnfc,
 	.set_rxnfc		= dsa_slave_set_rxnfc,
+	.get_ts_info		= dsa_slave_get_ts_info,
 };
 
 static const struct net_device_ops dsa_slave_netdev_ops = {
